fc30a5447e62ae3aba943e0e21f208f2bffc1d4c
Oleksandr <bobiksanya@gmail.com>
2023-11-14 20:30:15 +0200

    Add project files.

---
+++ git_lesson3/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace git_lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double operand1, operand2, result;
+
+            Console.WriteLine("Введіть перше число: ");
+            operand1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге число: ");
+            operand2 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Введіть знак арифметичної операції: ");
+            string sign = Console.ReadLine();
+
+            switch (sign)
+            {
+                case "+":
+                    {
+                        result = operand1 + operand2;
+                        Console.WriteLine($" Сума чисел = {operand1} + {operand2} = {result}");
+                        break;
+                    }
+                case "-":
+                    {
+                        result = operand1 - operand2;
+                        Console.WriteLine($" Різниця чисел = {operand1} - {operand2} = {result}");
+                        break;
+
+                    }
+                case "*":
+                    {
+                        result = operand1 * operand2;
+                        Console.WriteLine($" Множник чисел = {operand1} * {operand2} = {result}");
+                        break;
+                    }
+                case "/":
+                    {
+                        if (operand2 == 0)
+                        {
+                            Console.WriteLine("На нуль ділити не мона!!!");
+                        }
+                        else
+                        {
+                            result = operand1 / operand2;
+                            Console.WriteLine($" Частка чисел = {operand1} / {operand2} = {result}");
+
+
+                        }
+
+                        break;
+
+                    }
+
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
